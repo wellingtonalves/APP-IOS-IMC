a4eac542d88034b030af9dedf46a8172f334e044
Wellington Alves Gomes <wellington.alves.gomes@gmail.com>
2021-06-30 13:54:27 -0300

    Initial Commit

---
+++ IMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Wellington Alves on 30/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
